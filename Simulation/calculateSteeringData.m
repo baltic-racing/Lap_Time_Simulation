c5ba579b35c26ccf92b69126e700c1c0c7a19380
xCaivio <patricksiuta@gmail.com>
2021-10-21 14:38:48 +0200

    new version

--- Simulation/calculateSteeringData.m
+++ Simulation/calculateSteeringData.m
@@ -4,7 +4,7 @@
 % By Eric Dornieden / Patrick Siuta, Baltic Racing
 % Copyright (C) 2021, Baltic Racing, all rights reserved.
 
-function [delta, beta, psi1, alpha_f, alpha_r, alpha_fr, alpha_fl, alpha_rr, alpha_rl] = calculateSteeringData(wheelbase, R, lr, lf, vV, FWZ_fl, FWZ_rl,FWZ_fr,FWZ_rr)
+function [delta, beta, psi1, alpha_f, alpha_r, alpha_fr, alpha_fl, alpha_rr, alpha_rl ] = calculateSteeringData(wheelbase, R, lr, lf, vV, FWZ_fl, FWZ_rl,FWZ_fr,FWZ_rr)
     
     %% Check corner direction (left / right corner)
     if R > 0
@@ -18,9 +18,20 @@ function [delta, beta, psi1, alpha_f, alpha_r, alpha_fr, alpha_fl, alpha_rr, alp
     delta = f*atan((wheelbase/1000)/sqrt(R^2-(lr/1000)^2));   % [rad] Lenkwinkel
     beta = f*atan((lr/1000)/sqrt(R^2-(lr/1000)^2));  % [rad] Schwimmwinkel
     psi1 = vV/R;                               % [rad/s] Gierrate
-    alpha_f = 180/pi*(delta-(lf/1000)/vV*psi1-beta);  % [�] Schr�glaufwinkel vorne
+    alpha_f = 180/pi*((beta+((lf/1000) *psi1 )/vV)-delta);  % [degree] Slipangle Front (Formula 5.4 S148 / Race Car Vehicle Dynamics) 
     alpha_r = 180/pi*((lr/1000)/vV*psi1-beta);           % [�] Schr�glaufwinkel hinten
     
+%     if  delta < 0
+%        delta_fl= atan(wheelbase/(R-Track/2));                 % [degree] Steeringangle inner wheel
+%     else    
+%         delta > 0;
+%         delta_fl = atan(wheeelbase/(R+Track/2));
+%     end
+    
+    
+    alpha_fr = (FWZ_fr/FWZ_rr)*alpha_f;                 %[degree] Slipangle front right
+    
+    alpha_fl = (FWZ_fl/FWZ_rl)*alpha_f;                 %[degree] Slipangle front left
 
     if FWZ_fr/FWZ_rr >1                                 %[degree] Slipangle rear right
         alpha_rr = ((1/(FWZ_fr/FWZ_rr))*alpha_r);
