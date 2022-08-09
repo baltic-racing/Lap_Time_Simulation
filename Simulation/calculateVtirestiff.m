3f61dae5ccc2c47dffd48770149f3a5c21287271
Eric <e.dornieden@web.de>
2022-08-09 22:20:44 +0200

    Added Suspension kinematic calculation test

--- Simulation/calculateVtirestiff.m
+++ Simulation/calculateVtirestiff.m
@@ -4,11 +4,11 @@
 % By Eric Dornieden, Baltic Racing
 % Copyright (C) 2021, Baltic Racing, all rights reserved.
 
-function [cZ_fl, cZ_fr, cZ_rl, cZ_rr] = calculateVtirestiff(Fz, cZ_tire, FWZ_vl, FWZ_vr, FWZ_hl, FWZ_hr)
+function [cZ_fl, cZ_fr, cZ_rl, cZ_rr] = calculateVtirestiff(Fz, cZ_tire, FWZ_fl, FWZ_fr, FWZ_rl, FWZ_rr)
 %% Vertical tire stiffness
 
-        cZ_fl = interp1(Fz,cZ_tire,FWZ_vl,'linear','extrap'); % [N/m] Interpolated tire stiffness (Reifensteifigkeit interpoliert)
-        cZ_fr = interp1(Fz,cZ_tire,FWZ_vr,'linear','extrap'); % [N/m] Interpolated tire stiffness (Reifensteifigkeit interpoliert)
-        cZ_rl = interp1(Fz,cZ_tire,FWZ_hl,'linear','extrap'); % [N/m] Interpolated tire stiffness (Reifensteifigkeit interpoliert)
-        cZ_rr = interp1(Fz,cZ_tire,FWZ_hr,'linear','extrap'); % [N/m] Interpolated tire stiffness (Reifensteifigkeit interpoliert)       
+        cZ_fl = interp1(Fz,cZ_tire,FWZ_fl,'linear','extrap'); % [N/m] Interpolated tire stiffness (Reifensteifigkeit interpoliert)
+        cZ_fr = interp1(Fz,cZ_tire,FWZ_fr,'linear','extrap'); % [N/m] Interpolated tire stiffness (Reifensteifigkeit interpoliert)
+        cZ_rl = interp1(Fz,cZ_tire,FWZ_rl,'linear','extrap'); % [N/m] Interpolated tire stiffness (Reifensteifigkeit interpoliert)
+        cZ_rr = interp1(Fz,cZ_tire,FWZ_rr,'linear','extrap'); % [N/m] Interpolated tire stiffness (Reifensteifigkeit interpoliert)       
 end
