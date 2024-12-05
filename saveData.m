bf5f54675b68252cbab9dc088ce6616ec79baa9c
taino69 <147879098+taino69@users.noreply.github.com>
2024-12-05 20:02:06 +0100

    man kann jetzt kinematikpunkte speichern

--- saveData.m
+++ saveData.m
@@ -42,6 +42,9 @@ function saveData(app)
         setup.h_rc_f = app.h_rc_f;
         setup.h_rc_r = app.h_rc_r;
         setup.tirFile = convertCharsToStrings(app.tirFile);
+        setup.KinematicPointsFront = app.KinematicPointsFront;
+        %setup.CHAS_LowForFront = app.CHAS_LowForFront;
+
 
         %% Drivetrain Parameters
         setup.ptype = app.ptype;
